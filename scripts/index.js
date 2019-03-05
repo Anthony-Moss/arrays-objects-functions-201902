f452ffe5019329cf7c7305d179bf16f619466f21
Anthony Moss <asmoss93@gmail.com>
2019-03-05 15:37:15 -0500

    adds coolCities function that takes array of cities and returns cities whose temp cooler than 70degrees

--- scripts/index.js
+++ scripts/index.js
@@ -24,5 +24,22 @@ function squareNumbers(numbers) {
 
 squareNumbers(arrayOne)
 
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+];
 
-function cities
+function coolCities(citiesArray) {
+    let cities = (citiesArray)
+    let coldCities = []
+    cities.forEach(function (city) {
+    let citytemp = city.temperature
+    if (citytemp < 70) {
+        coldCities.push(city)
+    }
+})
+return coldCities
+}
+coolCities(cities)
