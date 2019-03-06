1ba547749eede3387337f9bb5ba73e066cb76cb5
Anthony Moss <asmoss93@gmail.com>
2019-03-06 09:16:29 -0500

    adds fixs that chris suggested in comments

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,12 @@
 arrayOne = [1,2,3,4,5,6,7,8,9,10,-1,-2,-3,20,99,97,200, 220, 12];
 
 function evenNumbers(numbers) {
-    let evenArray = numbers;
-    return evenArray.filter(i => i % 2 === 0);
+    return numbers.filter(i => i % 2 === 0);
 }
 console.log(evenNumbers(arrayOne))
 
 function positiveNumbers(numbers) {
-    let positiveArray = numbers;
-    return positiveArray.filter(i => i > 0)
+    return numbers.filter(i => i > 0)
 }
 console.log(positiveNumbers(arrayOne))
 
@@ -46,12 +44,11 @@ coolCities(cities)
 
 function cityNames(citiesArray) {
     let cityNameArray = []
-    let allCities = Object.values(citiesArray);
-    allCities.forEach(function (cities) {
-    let cityName = cities.name
-    cityNameArray.push(cityName)
-})
-return cityNameArray
+    citiesArray.forEach(function (cities) {
+        let cityName = cities.name
+        cityNameArray.push(cityName)
+    });
+    return cityNameArray
 }
 cityNames(cities)
 
